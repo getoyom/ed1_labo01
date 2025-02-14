ae96155d2fc09b68774f16a15a0085669b47914a
getoyom <getoyom@correo.url.edu.gt>
2025-02-14 08:18:57 -0600

    mensaje cualquiera

--- src/main/java/ed/lab/SortingTester.java
+++ src/main/java/ed/lab/SortingTester.java
@@ -38,7 +38,7 @@ public class SortingTester<T extends Comparable<T>> {
 
 
         System.out.printf("\t\tTiempo promedio: %s ms\n", average);
-        System.out.printf("\t\tTiempo total: %s ms\n", sum);
+        System.out.printf("\t\tTiempo total: %s ms\n", sum);//iulyhilu
     }
 }
 }
